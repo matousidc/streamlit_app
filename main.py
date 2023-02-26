eabdcb220c1d9c6fa7814374e290b6bd42fd02b9
Slonek, Matous (mx429g) <mx429g@att.com>
2023-02-26 21:17:59 +0100

    data, altair plotting

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def get_price(coin: str, vs_coin: str) -> str:
     """
     Simple price of coin versus another coin
     """
-    params = {"ids": coin, "vs_currencies": vs_coin, "include_market_cap": 'true', "include_24hr_change": 'true'}
+    params = {
+        "ids": coin,
+        "vs_currencies": vs_coin,
+        "include_market_cap": "true",
+        "include_24hr_change": "true",
+    }
     r = requests.get("https://api.coingecko.com/api/v3/simple/price", params=params)
     # coins and vs currencies lists
     print(r.json())
@@ -20,27 +25,33 @@ def get_price(coin: str, vs_coin: str) -> str:
 
 def coin_names():  # TODO: save coins into pickle, api time limit, check like once a day
     resp_coins = requests.get("https://api.coingecko.com/api/v3/coins/list")
-    resp_vscoins = requests.get("https://api.coingecko.com/api/v3/simple/supported_vs_currencies")
+    resp_vscoins = requests.get(
+        "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+    )
     # print(len(resp.json()))
     # print(len(r.json()))
     return resp_coins.json(), resp_vscoins.json()
 
 
-@st.cache
-def coin_history(coin: str, vs_coin: str, days_back: int, interval: int | None = None) -> pd.DataFrame:
+@st.cache_data
+def coin_history(
+    coin: str, vs_coin: str, days_back: int, interval: int | None = None
+) -> pd.DataFrame:
     """
     Creates dataframe with coin history, interval is either automatic or can be specified
     """
     params = {"vs_currency": vs_coin, "days": days_back, "interval": interval}
-    r = requests.get(f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart", params=params)
+    r = requests.get(
+        f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart", params=params
+    )
     df = pd.DataFrame(r.json())
-    df['timestamp'] = df['prices'].apply(lambda x: x[0])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    df['timestamp'] = df['timestamp'].dt.strftime("%Y-%m-%d %H")
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['prices'] = df['prices'].apply(lambda x: x[1])
-    df['market_caps'] = df['market_caps'].apply(lambda x: x[1])
-    df['total_volumes'] = df['total_volumes'].apply(lambda x: x[1])
+    df["timestamp"] = df["prices"].apply(lambda x: x[0])
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+    df["timestamp"] = df["timestamp"].dt.strftime("%Y-%m-%d %H")
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df["prices"] = df["prices"].apply(lambda x: x[1])
+    df["market_caps"] = df["market_caps"].apply(lambda x: x[1])
+    df["total_volumes"] = df["total_volumes"].apply(lambda x: x[1])
     # print(df.head())
     return df
 
@@ -49,7 +60,7 @@ def plot_history(df: pd.DataFrame):
     """
     Line plot from dataframe with coin history
     """
-    sns.lineplot(df, x='timestamp', y="prices")
+    sns.lineplot(df, x="timestamp", y="prices")
     plt.show()
 
 
@@ -61,41 +72,46 @@ def streamlit_header():
         menu_items={
             # 'Get Help': 'https://www.extremelycoolapp.com/help',
             # 'Report a bug': "https://www.extremelycoolapp.com/bug",
-            'About': "Made by https://github.com/matousidc!"
-        }
+            "About": "Made by https://github.com/matousidc!"
+        },
     )
-    st.title('Coin chart')
+    st.title("Coin chart")
     st.markdown("## Main page")
     # sidebar
     with st.sidebar:
         add_radio = st.radio(
-            "Choose a shipping method",
-            ("Standard (5-15 days)", "Express (2-5 days)")
+            "Choose a shipping method", ("Standard (5-15 days)", "Express (2-5 days)")
         )
+        print(add_radio)
 
 
 def streamlit_plotting(df, coin, vs_coin):
     st.markdown(f"### Coin history chart: {coin}/{vs_coin}")
-    st.line_chart(data=df, x='timestamp', y='prices')
+    st.line_chart(data=df, x="timestamp", y="prices")
+    # same plot but seaborn through plt.figure
     fig = plt.figure(figsize=(10, 4))
-    sns.lineplot(df, x='timestamp', y="prices")
+    sns.lineplot(df, x="timestamp", y="prices")
     st.pyplot(fig)
 
 
 def streamlit_options(coins: list[dict], vs_coins: list) -> tuple[str, str, int]:
-    with st.form("my_form"):  # form batching choices together, app is rerun after clicking on submit
+    # form batching choices together, app is rerun after clicking on submit
+    with st.form("my_form"):
         option = st.selectbox(
-            'What coin history do you want to see?',
-            tuple(f"{x['name']}-${x['symbol']}" for x in coins))
-        vs_option = st.selectbox(
-            'Against which coin?',
-            tuple(vs_coins))
+            "What coin history do you want to see?",
+            tuple(f"{x['name']}-${x['symbol']}" for x in coins),
+        )
+        vs_option = st.selectbox("Against which coin?", tuple(vs_coins))
         interval = st.radio(
             "How far back do you want to display the history?",
-            ('1 day', '1 week', '1 month', '3 months'))
-        interval_match = {x: y for x, y in zip(('1 day', '1 week', '1 month', '3 months'), (1, 7, 30, 90))}
+            ("1 day", "1 week", "1 month", "3 months"),
+        )
+        interval_match = {
+            x: y
+            for x, y in zip(("1 day", "1 week", "1 month", "3 months"), (1, 7, 30, 90))
+        }
         days_back = interval_match[interval]
-        coins_dict = {f"{x['name']}-${x['symbol']}": x['id'] for x in coins}
+        coins_dict = {f"{x['name']}-${x['symbol']}": x["id"] for x in coins}
         option = coins_dict[option]
         st.form_submit_button("Submit")
     return option, vs_option, days_back
@@ -111,10 +127,10 @@ def main():
     option, vs_option, days_back = streamlit_options(coins, vs_coins)
     df_history = coin_history(option, vs_option, days_back)
     streamlit_plotting(df_history, option, vs_option)
-    print('rerun')
+    print("rerun")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 # df.query('@df > s and sdf <= sd'), chaining methods
---
+++ pages/1_Cycling_data.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+import statsmodels.formula.api as smf
+
+
+# setup page style
+st.set_page_config(
+    page_title="My app-cycling_data",
+    page_icon="🧊",
+    initial_sidebar_state="auto",
+    menu_items={
+        # 'Get Help': 'https://www.extremelycoolapp.com/help',
+        # 'Report a bug': "https://www.extremelycoolapp.com/bug",
+        "About": "Made by https://github.com/matousidc!"
+    },
+)
+st.title("Brno cycling statistics")
+
+
+@st.cache_data
+def load_df_dpnk():
+    """Loads 'to work by bike' dataset"""
+    df_dpnk = pd.read_csv("data_project3\dpnk.csv")
+    return df_dpnk
+
+
+@st.cache_data
+def load_df_roads():
+    """Loads and cleans cycling infrastructure dataset"""
+    df_roads = pd.read_csv("data_project3/Cykloopatreni.csv")
+    # length in metres
+    # deletes rows with year == 0
+    df_roads = df_roads.drop(df_roads[df_roads["rok_realizace"] == 0].index)
+    # year into int type
+    df_roads["rok_realizace"] = df_roads["rok_realizace"].astype("Int64")
+    # zrušená infrastruktura vyjádřena zápornou délkou, příprava pro lineární regresi
+    df_roads.loc[df_roads[df_roads["typ_opatreni"] == "Úsek byl zrušen"].index, "delka"] = (
+        df_roads["delka"].loc[df_roads[df_roads["typ_opatreni"] == "Úsek byl zrušen"].index] * -1
+    )
+    df_roads.loc[df_roads[df_roads["typ_opatreni"] == "Vjezd cyklistům zakázán"].index, "delka"] = (
+        df_roads["delka"].loc[df_roads[df_roads["typ_opatreni"] == "Vjezd cyklistům zakázán"].index] * -1
+    )
+    return df_roads
+
+
+@st.cache_data
+def infrastructure_progress():
+    df_realizace = df_roads[["rok_realizace", "delka"]].groupby(["rok_realizace"]).sum().reset_index()
+    df_realizace.loc[-1] = [1996.0, 0.0]
+    df_realizace.sort_values(by=["rok_realizace"], inplace=True, ignore_index=True)
+    df_realizace["total_length"] = df_realizace["delka"].cumsum() / 1000
+    df_realizace.columns = ["year", "length", "total_length"]
+    return df_realizace
+
+
+@st.cache_data
+def get_reg_fit(data: pd.DataFrame, yvar: str, xvar: str, alpha=0.05) -> tuple[pd.DataFrame, alt.Chart]:
+    # Grid for predicted values
+    x = data.loc[pd.notnull(data[yvar]), xvar]
+    grid = np.arange(x.min(), x.max() + 0.1)
+    predictions = pd.DataFrame({xvar: grid})
+    data[xvar] = data[xvar].astype("float64")
+
+    # Fit model, get predictions
+    model = smf.ols(f"{yvar} ~ {xvar}", data=data).fit()
+    model_predict = model.get_prediction(predictions[xvar])
+    predictions[yvar] = model_predict.summary_frame()["mean"]
+    predictions[["ci_low", "ci_high"]] = model_predict.conf_int(alpha=alpha)
+    # predictions[xvar] = predictions[xvar].astype("Int64")
+
+    # Build chart
+    reg = alt.Chart(predictions).mark_line(color="darkorange", opacity=0.8).encode(x=f"{xvar}:O", y=f"{yvar}:Q")
+    ci = (
+        alt.Chart(predictions)
+        .mark_errorband(color="darkorange", opacity=0.2)
+        .encode(
+            x=f"{xvar}:O",
+            y=alt.Y("ci_low:Q", title="Total infrastructure length [km]"),
+            y2="ci_high:Q",
+        )
+    )
+    chart = ci + reg
+    return predictions, chart
+
+
+# ============================= defining plots
+# scatter plot with regression line
+df_dpnk = load_df_dpnk()
+df_roads = load_df_roads()
+df_realizace = infrastructure_progress()
+chart = (
+    alt.Chart(df_realizace)
+    .mark_point(color="red", size=60, filled=True)
+    .encode(
+        x=alt.X("year:O", title="Year"),
+        y=alt.Y("total_length:Q", scale=alt.Scale(domain=(-20, 140))),
+    )
+)
+# y=alt.Y("total_length:Q", axis=alt.Axis(title="Total infrastructure length [km]")),
+
+polynomial_fit = chart.transform_regression("year", "total_length").mark_line(color="darkorange")
+progress_chart = (
+    alt.Chart(df_realizace)
+    .mark_bar()
+    .encode(
+        x=alt.X("year:O", axis=alt.Axis(title="Year")),
+        y=alt.Y("length:Q", axis=alt.Axis(title="Length of new infrastructure [m]")),
+        color=alt.Color("length", scale=alt.Scale(scheme="goldred"), legend=alt.Legend(title="Length [m]")),
+    )
+)
+fit, reg_chart = get_reg_fit(df_realizace, yvar="total_length", xvar="year", alpha=0.05)
+# ============================= writing to streamlit
+# another arguments into alt.Chart
+# x="year:O",
+# y="total_length:Q",
+# scale=alt.Scale(domain=(0, 140)),
+# .configure_mark(color="red")
+# .interactive()
+# band = alt.Chart(chart).mark_errorband(extent="ci").encode(x="year:O", y=alt.Y("total_length:Q")) # doesnt work on transform_regression
+# scheme="magma" nice but low values not visible on dark background
+# rebeccapurple nice purple
+
+
+st.markdown("### 'To work by bike' dataset")
+st.dataframe(df_dpnk)
+st.markdown("### Cycling infrastructure dataset")
+st.dataframe(df_roads)
+st.markdown("### Progress of total cycling infrastructure length with fitted linear regression")
+st.altair_chart(alt.layer(chart, polynomial_fit), use_container_width=True)
+st.altair_chart(chart + reg_chart, use_container_width=True)
+st.markdown("### Length of new cycling infrastructure built each year")
+st.altair_chart(progress_chart, use_container_width=True)
